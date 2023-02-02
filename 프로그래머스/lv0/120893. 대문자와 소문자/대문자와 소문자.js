4d99133d160db332b0b00718cdfbcb60bcfe2290
Yeji Choi <114912732+examplezi@users.noreply.github.com>
2023-02-02 22:02:06 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,12 +1,4 @@
 function solution(my_string) {
     var answer = '';
-    for(let i = 0; i <my_string.length ; i++){
-        if(my_string[i] !== my_string[i].toLowerCase()){
-            answer = answer + my_string[i].toLowerCase()
-        }
-        else{
-            answer = answer + my_string[i].toUpperCase()
-        }
-    }
-    return answer
+    return answer;
 }
