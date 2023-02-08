28cad0d4d70208adfdb0efe640418873ac4c7a34
Yeji Choi <114912732+examplezi@users.noreply.github.com>
2023-02-09 00:48:36 +0900

    Update k의 개수.js

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -2,21 +2,19 @@ function solution(i, j, k) {
     var answer = '';
   let sum= []
   let char = String(k)
- // console.log(char)
-//  let sum = 0;
-   // return answer;
+
   for(let a = i; a <= j; a++){
-   // console.log(a)
+  
     answer += a
-   // console.log(a)
+
   }
 let arr = answer.split('')
- // console.log(arr)
+ 
   for(let b = 0; b < arr.length; b++){
-  // console.log(arr[b])
+ 
      if(arr[b].includes(char)){
+         // arr 비교가 아닌 arr[b] 하나씩 비교 
    sum.push(arr[b])
- // console.log(sum)
   }
   }
   
