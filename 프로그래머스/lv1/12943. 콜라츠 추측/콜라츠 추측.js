ab5786760a3a0eca5638073c80c228eb98b294c3
Yeji Choi <114912732+examplezi@users.noreply.github.com>
2023-02-23 23:12:34 +0900

    Update 콜라츠 추측.js

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -13,3 +13,19 @@ for( let i = 0; i < 500; i++ ) {
   }
     return -1;
 }
+
+// function solution(num) { 
+
+// for( let i = 0; i < 500; i++ ) {
+//        if(num%2==0){
+//       num = num/2;
+//     }
+//     else if(num==1){ // 최종 횟수 반환 
+//         return i;
+//     }
+//     else if(num%2==1){ // 홀수
+//       num = (num*3)+1;
+//     }
+//   }
+//     return -1;
+// }
