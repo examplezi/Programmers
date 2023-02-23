9c1b90d2ea0b4c871fa23d3c5862f9aec2658f94
Yeji Choi <114912732+examplezi@users.noreply.github.com>
2023-02-23 22:35:37 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,13 @@
+function solution(n) {
+for(let i = 1 ; i <= n ; i++){
+  //  console.log(i * i)
+    while(i * i === n){
+    return (i + 1) * (i + 1)
+      break
+  }
+    
+  }
+  return -1
+}
+    
+  
