9ca8524ca3485633e794c06cce2099a0570c67f9
Yeji Choi <114912732+examplezi@users.noreply.github.com>
2023-02-24 19:39:56 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.85 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,8 @@
-function solution(phone_number) {
-     let answer = phone_number.slice(-4)
+function solution(phone_number) { 
+let answer = phone_number.slice(-4)
   console.log(answer)
- return answer.padStart(phone_number.length, '*');
+ let result = "*".repeat(phone_number.length-4)
+ return result + answer
+
+
 }
