2c4ef3b2926aa21b9ee210270c3f22f1c8b82d4e
Yeji Choi <114912732+examplezi@users.noreply.github.com>
2023-03-03 01:10:06 +0900

    [Bronze V] Title: 나머지, Time: 128 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+var A = parseInt(input[0]);
+var B = parseInt(input[1]);
+var C = parseInt(input[2])
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력
+console.log((A+B)%C);
+console.log(((A%C) + (B%C))%C);
+console.log((A*B)%C);
+console.log(((A%C) * (B%C))%C);
