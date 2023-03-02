0d5b30284f956be2a1ffdd8d9c17cb418972a1f0
Yeji Choi <114912732+examplezi@users.noreply.github.com>
2023-03-03 00:57:31 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+//첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(parseInt(a/b));
+console.log(a%b);
