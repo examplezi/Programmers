b99a49531747b210c8c1ce63e8beba94329386c2
Yeji Choi <cyei0824@gmail.com>
2023-07-18 12:07:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 131.81 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,7 +2,7 @@ function solution(arr)
 {
     var answer = [];
     for(let i =0; i < arr.length; i++){
-        if( arr[i] !== arr[i + 1] ) {
+        if( arr[-1] !== arr[i] ) {
             answer.push( arr[i] )
         }
     }
