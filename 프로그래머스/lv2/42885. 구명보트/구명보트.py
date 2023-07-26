84d4cd36543e7d62987290b815138150a0c4dcd3
Yeji Choi <cyei0824@gmail.com>
2023-07-26 23:32:08 +0900

    [level 2] Title: 구명보트, Time: 11.17 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+# 구명보트 최소 개수, 한 보트 당 최대 2명 
+from collections import deque
+def solution(people, limit):
+    answer = 0
+    deq = deque(sorted(people))
+    while deq:
+        if len(deq) == 1:
+            answer += 1
+            break
+        if deq[0] + deq[-1] <= limit:
+            deq.pop()
+            deq.popleft()
+        else:
+            deq.pop()
+        answer += 1
+    return answer
+        
