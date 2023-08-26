a545779fa2accc14e030def089ea8656ac1bb060
Yeji Choi <cyei0824@gmail.com>
2023-08-27 02:06:42 +0900

    [level 1] Title: 폰켓몬, Time: 0.43 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -13,13 +13,21 @@
 
 # set로 중복값 제거해서 그걸 배열 길이의 반까지 돌게해서 +1 씩 
 
-def solution(nums):
-    answer = 0
-    length = len(nums) //2
-    kind = list(set(nums))
-    print(kind)
+# def solution(nums):
+#     answer = 0
+#     length = len(nums) //2
+#     kind = list(set(nums))
+#     print(kind)
     
-    for i in kind:
-        if(answer < length):
-            answer += 1
-    return answer
+#     for i in kind:
+#         if(answer < length):
+#             answer += 1
+#     return answer
+
+def solution(nums):
+    choice = len(nums) / 2
+    #print(choice, set(nums), len(set(nums)))
+    if choice <= len(set(nums)):
+        return choice
+    else:
+        return len(set(nums))
