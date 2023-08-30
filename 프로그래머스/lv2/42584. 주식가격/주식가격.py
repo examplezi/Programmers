03a5fb3907411da0c7940b1b2f8a22bd1afb7834
Yeji Choi <cyei0824@gmail.com>
2023-08-31 01:27:01 +0900

    [level 2] Title: 주식가격, Time: 34.21 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,36 +1,30 @@
-# from collections import deque
-
-# def solution(prices):
-#     answer = [] #deque([0 for _ in range(prices)])
-#     queue = deque(prices)
+from collections import deque
+'''
+queue에 하나씩 넣음 
+prices에 current보다 작은 요소가 나오면, 작은 요소까지의 거리 len(prices)
+current 
+'''
+def solution(prices):
+    answer = [] #deque([0 for _ in range(prices)])
+    queue = deque(prices)
+    while queue:
+        current = queue.popleft()
+        count = 0
+        for i in queue:
+            count += 1
+            if current > i:
+                break
+        answer.append(count)
+    return answer
 #     while queue:
 #         current = queue.popleft()
 #         length = 0
 
-#         for i in range(len(queue)): # current 보다 작은 수까지 범위
-#             if queue[i] >= current:
-             
-#                 length += 1
-             
-#             else: # 현재보다 작다면
-#                 length += 1
-#                 break
+#         for i in queue:
+#             length += 1
+#             if i < current:
+#                     break
 #         answer.append(length)
 #     return answer
 
-from collections import deque
-
-def solution(prices):
-    queue = deque(prices)
-    answer = []
-    
-    while queue:
-        price = queue.popleft()
-        sec = 0
-        for q in queue:
-            sec += 1
-            if price > q:
-                break 
-        answer.append(sec)        
-    return answer
         
