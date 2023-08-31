3251f1c40d7bd53c501e50aa4164d8e8615ff1aa
Yeji Choi <cyei0824@gmail.com>
2023-08-31 19:47:38 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,20 +1,35 @@
-def solution(array, commands):
+# def solution(array, commands):
+#     answer = []
+#     for i in range(len(commands)):
+#         start = commands[i][0]
+#         stop = commands[i][1]
+#         target = commands[i][2]
+#         result = array[start-1:stop]
+#         result.sort()
+#         for j in range(len(result)):
+#             if j == target-1:
+#                 answer.append(result[j])
+#     return answer
+# def solution(array, commands):
+#     answer = []
+#     for i in range(len(commands)):
+#         start = commands[i][0]
+#         stop = commands[i][1]
+#         target = commands[i][2]
+#         result = array[start-1:stop]
+#         result.sort()
+#         for j in range(len(result)):
+#             if j == target-1:
+#                 answer.append(result[j])
+#     return answer
+
+def solution(array,commands):
     answer = []
-    for i in range(len(commands)):
-        start = commands[i][0]
-        stop = commands[i][1]
-        target = commands[i][2]
-        print(target)
-        result = array[commands[i][0]-1:commands[i][1]]
-        result.sort()
-        print(result)
-        for j in range(len(result)):
-            print(j, target-1)
-            if j == target-1:
-                answer.append(result[j])
-                print(answer)
+
+    for i in commands:
+        ary = array[i[0]-1 : i[1]] #문제에서 주어진 크기만큼 자르기
+
+        ary.sort() # 오름차순
+        answer.append(ary[i[2]-1]) # k번째 값 넣기 
 
     return answer
-            
-            
-        #print(result)
