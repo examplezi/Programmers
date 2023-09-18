7302cc5b2c48cda075e69e81476e2ba3d77f3ee7
Yeji Choi <cyei0824@gmail.com>
2023-09-18 23:54:26 +0900

    [level 3] Title: 네트워크, Time: 0.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,19 +1,17 @@
 def solution(n, computers):
     answer = 0
-    visited = [False] * n
-    
-    def dfs(v): # 노드 안 
-        visited[v] = True # 셀프 루프 제거 
-        for j in range(n):
-            if computers[v][j] == 1 and visited[j] == False:
-                dfs(j) # 다음 연결된 노드로 가려면 반복 증가하는 j
-    
-    
-    for i in range(n):
-        #print(i)
-        if visited[i] == False: #방문이 안되어 있으면
-            dfs(i) # 안에 들어가서 살펴봐라, dfs가 끝나면 다시 돌아옴
-            answer += 1 # 연결된 노드 모두 조사 후에 + 1
+    queue = []
+    visited = []
+    for a in range(n):
+        if a not in visited:
+            queue.append(a)
+            answer += 1
+            print(queue)
+            while queue : # 큐가 빌때까지 반복
+                now = queue.pop(0)    
+                for i in range(n):
+                    if computers[now][i] == 1 and i not in visited:
+                        visited.append(i)
+                        queue.append(i)
+                        print(visited,queue)
     return answer
-    
-    
