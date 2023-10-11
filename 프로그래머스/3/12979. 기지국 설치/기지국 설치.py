23502eef76b5e22ed1b2775acfa5d04b8820738c
Yeji Choi <cyei0824@gmail.com>
2023-10-12 01:37:42 +0900

    [level 3] Title: 기지국 설치, Time: 2.49 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,19 @@
+from math import ceil
+
+def solution(n, stations, w):
+    answer = 0
+    s = 1
+
+    for station in stations:
+        e = station - w
+        total = e - s
+        if e > 1 and total > 0:
+            answer += ceil(total / ((2 * w) + 1))
+
+        s = station + w + 1
+
+    if s <= n:
+        total = (n + 1) - s
+        answer += ceil(total / ((2 * w) + 1))
+
+    return answer
