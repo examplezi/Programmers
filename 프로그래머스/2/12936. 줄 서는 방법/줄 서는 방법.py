b5e6a341a34ba2120d9c93b91a6e5a1d6b17dfda
Yeji Choi <cyei0824@gmail.com>
2023-10-19 20:52:52 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,44 +1,16 @@
-# #from itertools import permutations
-# def solution(n, k):
-#     answer = []
-#     arr = []
-#     for i in range(1,n+1):
-#         arr.append(i)
-# #     for i in permutations(arr,n):
-# #         answer.append(list(i))
-
-# #     for i in range(len(answer)):
-# #         if i+1 == k:
-# #             return answer[i]
-    
-    
-# #     itertools 사용 x (n이 커질수록 시간 초과)
-
-# # factorial list로 순회, k의 몫과 나머지를 사용 divmod
-
-# 이 문제는 팩토리얼로 전체 경우의 수를 구하고,
-# 거기서 범위를 연산해서 순서를 구해야 합니다.
-
-def factorial(n):
-    if n < 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
+from math import factorial
 def solution(n, k):
-    # 총 n!의 경우의 수를 갖고있음
-    # 맨 앞의 수가 정해지면 (n - 1)! 의 경우의 수를 가짐
-    # k에서 (n - 1)!을 나눴을 때의 몫이 첫번째 오는 자리
-    # k에서 (n - 1)!을 나눴을 때의 나머지가 다시 k로 오는 자리
-    result = []
-    num_list = [i for i in range(1, n + 1)]
-    while(n != 0):
-        num_case = factorial(n - 1)
-        idx = k // num_case
-        k = k % num_case
-        if k == 0:
-            result.append(num_list.pop(idx - 1))
-        else:
-            result.append(num_list.pop(idx))
-        n -= 1
-    return result
+    answer = []
+    nums = list(range(1, n+1))
+    order = []
+    k -= 1
+    for i in range(n-1, -1, -1):
+        temp = factorial(i)
+        q, r = divmod(k, temp)
+        k = r
+        order.append(q)
+    # print(order)
+    for idx in order:
+        answer.append(nums.pop(idx))
+
+    return answer
