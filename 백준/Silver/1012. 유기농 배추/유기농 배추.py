70db33abcc8ecc89800f549481be009e82e3455f
Yeji Choi <cyei0824@gmail.com>
2023-11-12 17:10:53 +0900

    [Silver II] Title: 유기농 배추, Time: 160 ms, Memory: 114312 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,46 +1,42 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
+T = int(input())
 
-def bfs(x, y):
-    q = deque([(x, y)])
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
     while q:
-        x, y = q.popleft()
+        x,y = q.popleft()
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
 
         for i in directions:
             nx = x + i[0]
             ny = y + i[1]
-            
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1: # 인접하는지 체크 
-                q.append((nx, ny))
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                q.append((nx,ny))
                 graph[nx][ny] = 2
-    #print(graph, "체크하고나서")
-    
     return 1
 
-T = int(input())
-for i in range(T):
+
+
+
+
+
+
+for _ in range(T):
     n,m,k = map(int, input().split())
     graph = [[0] * m for _ in range(n)]
-    #print(graph)
-    #arr = list(map(int,input().split()))
-    cab = [list(map(int, input().split())) for _ in range(k)]
-    visited = [0] * n
-    #print(cab)
-
-
-
-
-
-
-    for i, j in cab:
-        graph[i][j] = 1
-    #print(graph)
-    directions = [[0,1], [0,-1], [1,0], [-1,0]]
+    for i in range(k):
+        x, y = map(int, input().split())
+        graph[x][y] = 1
+    #   print(graph)
     cnt = 0
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 1:
-                cnt += bfs(i, j)
+                cnt += bfs(i,j)
 
     print(cnt)
